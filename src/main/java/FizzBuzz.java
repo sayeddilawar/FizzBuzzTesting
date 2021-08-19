571d4a56fd80e6c527ae08be2ac98da822f5ffcb
Sayeddilawar <sayeddilawar631@gmail.com>
2021-08-19 20:13:27 +0530

    Requirement 1 : Checked Whether Number is Fizz, Buzz or FizzBuzz.

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,19 @@
-public class FizzBuzz {
+public class FizzBuzz
+{
+    int number;
+    public FizzBuzz(int number)
+    {
+        this.number=number;
+    }
+    public String checkFizzOrBuzz() {
+        if(number>=1 && number<=100) {
+            if (number % 3 == 0)
+                return "Fizz";
+            else if(number %5 ==0)
+                return "Buzz";
+            else
+                return "FizzBuzz";
+        }
+        return "null";
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,36 @@
-public class FizzBuzzTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class FizzBuzzTest
+{
+
+    @Test
+    void toCheckNumberIsFizz()
+    {
+        FizzBuzz object = new FizzBuzz(3);
+        assertEquals("Fizz",object.checkFizzOrBuzz());
+    }
+
+    @Test
+    void toCheckNumberIsBuzz()
+    {
+        FizzBuzz object = new FizzBuzz(5);
+        assertEquals("Buzz",object.checkFizzOrBuzz());
+    }
+
+    @Test
+    void toCheckNumberIsFizzBuzz()
+    {
+        FizzBuzz object = new FizzBuzz(30);
+        assertNotEquals("FizzBuzz",object.checkFizzOrBuzz());
+    }
+
+    @Test
+    void toCheckNumberIsNull()
+    {
+        FizzBuzz object = new FizzBuzz(-2);
+        assertEquals("null",object.checkFizzOrBuzz());
+    }
 }
